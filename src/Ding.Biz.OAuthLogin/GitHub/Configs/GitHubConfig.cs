1fe15c3ac7fc2d1be71aa2e2d98c799bbdbc6a25
丁川 <100538511@qq.com>
2019-04-03 19:38:36 +0800

    更新

---
+++ src/Ding.Biz.OAuthLogin/GitHub/Configs/GitHubConfig.cs
@@ -0,0 +1,51 @@
+﻿namespace Ding.Biz.OAuthLogin.GitHub.Configs
+{
+    /// <summary>
+    /// 配置
+    /// </summary>
+    public class GitHubConfig
+    {
+        /// <summary>
+        /// 请根据步骤操作：authorize => access_token => user
+        /// </summary>
+        public enum Step
+        {
+            /// <summary>
+            /// 授权
+            /// </summary>
+            Step1_Authorize,
+            /// <summary>
+            /// token
+            /// </summary>
+            Step2_AccessToken,
+            /// <summary>
+            /// user
+            /// </summary>
+            Step3_User
+        }
+
+        #region API请求接口
+
+        /// <summary>
+        /// GET
+        /// </summary>
+        public string API_Authorize = "https://github.com/login/oauth/authorize";
+
+        /// <summary>
+        /// POST
+        /// </summary>
+        public string API_AccessToken = "https://github.com/login/oauth/access_token";
+
+        /// <summary>
+        /// GET
+        /// </summary>
+        public string API_User = "https://api.github.com/user";
+
+        #endregion
+
+        /// <summary>
+        /// github 申请的应用名称
+        /// </summary>
+        public string ApplicationName { get; set; } =  "";
+    }
+}
---
+++ src/Ding.Biz.OAuthLogin/GitHub/Configs/GitHubConfigProvider.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+
+namespace Ding.Biz.OAuthLogin.GitHub.Configs
+{
+    /// <summary>
+    /// QQ配置提供器
+    /// </summary>
+    public class GitHubConfigProvider : IGitHubConfigProvider
+    {
+        /// <summary>
+        /// 配置
+        /// </summary>
+        private readonly GitHubConfig _config;
+
+        /// <summary>
+        /// 初始化QQ配置提供器
+        /// </summary>
+        /// <param name="config"></param>
+        public GitHubConfigProvider(GitHubConfig config)
+        {
+            _config = config;
+        }
+
+        /// <summary>
+        /// 获取配置
+        /// </summary>
+        /// <returns></returns>
+        public Task<GitHubConfig> GetConfigAsync()
+        {
+            return Task.FromResult(_config);
+        }
+    }
+}
