78cc10e30afbb03b2cec399f7762f3922399e13b
丁川 <100538511@qq.com>
2019-04-08 20:58:04 +0800

    增加邮件类

---
+++ src/Ding.MailKit/IMailKitEmailSender.cs
@@ -0,0 +1,11 @@
+﻿using Ding.Net.Mail.Abstractions;
+
+namespace Ding.MailKit
+{
+    /// <summary>
+    /// MailKit电子邮件发送器
+    /// </summary>
+    public interface IMailKitEmailSender : IEmailSender
+    {
+    }
+}
