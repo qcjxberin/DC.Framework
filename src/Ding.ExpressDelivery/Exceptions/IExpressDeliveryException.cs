ddb8ddd4e3d0e170a4ccb1d3ffedbfe9d93b9534
丁川 <100538511@qq.com>
2019-04-12 23:28:54 +0800

    增加快递组件

---
+++ src/Ding.ExpressDelivery/Exceptions/IExpressDeliveryException.cs
@@ -0,0 +1,13 @@
+﻿namespace Ding.ExpressDelivery.Exceptions
+{
+    /// <summary>
+    /// 快递异常
+    /// </summary>
+    public interface IExpressDeliveryException
+    {
+        /// <summary>
+        /// 消息
+        /// </summary>
+        string Message { get; }
+    }
+}
---
+++ src/Ding.ExpressDelivery/IConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace Ding.ExpressDelivery
+{
+    /// <summary>
+    /// 快递配置
+    /// </summary>
+    public interface IConfig
+    {
+    }
+}
