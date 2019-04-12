f1432adea5bcdfefe821b050d4efa56b987c8e4a
丁川 <100538511@qq.com>
2019-04-12 22:01:56 +0800

    增加图像处理类

---
+++ src/Ding.Utils.Drawing/BitmapExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+
+namespace Ding.Utils.Drawing
+{
+    /// <summary>
+    /// 图像(<see cref="Bitmap"/>) 扩展
+    /// </summary>
+    public static class BitmapExtensions
+    {
+    }
+}
