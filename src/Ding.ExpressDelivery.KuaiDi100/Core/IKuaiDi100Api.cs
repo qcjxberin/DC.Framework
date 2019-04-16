bb180334df2cf1568bff89d00769ba06d49a0810
丁川 <100538511@qq.com>
2019-04-16 11:21:23 +0800

    转移快递的组件

---
+++ src/Ding.ExpressDelivery.KuaiDi100/Core/IKuaiDi100Api.cs
@@ -0,0 +1,6 @@
+﻿namespace Ding.ExpressDelivery.KuaiDi100.Core
+{
+    public interface IKuaiDi100Api
+    {
+    }
+}
---
+++ src/Ding.ExpressDelivery/IAccountSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Ding.ExpressDelivery
+{
+    /// <summary>
+    /// 账户设置
+    /// </summary>
+    public interface IAccountSettings
+    {
+    }
+}
