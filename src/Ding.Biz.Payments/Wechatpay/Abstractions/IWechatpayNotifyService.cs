a8fcf7010e269c6a80141a706e22ffbb269fbb1f
丁川 <100538511@qq.com>
2019-04-18 11:05:38 +0800

    移除支付组件

--- src/Ding.Biz.Payments/Wechatpay/Abstractions/IWechatpayNotifyService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Ding.Biz.Payments.Core;
-
-namespace Ding.Biz.Payments.Wechatpay.Abstractions {
-    /// <summary>
-    /// 微信支付通知服务
-    /// </summary>
-    public interface IWechatpayNotifyService : INotifyService {
-    }
-}
