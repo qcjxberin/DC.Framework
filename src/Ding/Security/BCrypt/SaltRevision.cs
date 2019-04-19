44b2eafbaf47765f312b6715b9b36f064f01de60
丁川 <100538511@qq.com>
2019-04-20 00:00:55 +0800

    调整

---
+++ src/Ding/Security/BCrypt/SaltRevision.cs
@@ -0,0 +1,11 @@
+﻿namespace Ding.Security.BCrypt
+{
+    public enum SaltRevision
+    {
+        Revision2,
+        Revision2A,
+        Revision2B,
+        Revision2X,
+        Revision2Y
+    }
+}
