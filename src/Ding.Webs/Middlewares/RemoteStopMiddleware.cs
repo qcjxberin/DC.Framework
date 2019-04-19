d667db70433de30c2b002f49706fe4b5b8b3045f
丁川 <100538511@qq.com>
2019-04-19 17:52:31 +0800

    增加远程关闭系统中间件

---
+++ src/Ding.Webs/Middlewares/RemoteStopMiddleware.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ding.Webs.Middlewares
+{
+    /// <summary>
+    /// 远程关闭系统中间件
+    /// 发起请求的Header头上加上Stop-Application:Yes
+    /// </summary>
+    public class RemoteStopMiddleware
+    {
+        /// <summary>
+        /// 方法
+        /// </summary>
+        private RequestDelegate _next;
+
+        /// <summary>
+        /// 请求时Header中附带的参数名
+        /// </summary>
+        private const string RequestHeader = "Stop-Application";
+
+        /// <summary>
+        /// 响应时Header中附带的参数名
+        /// </summary>
+        private const string ResponseHeader = "Application-Stopped";
+
+        /// <summary>
+        /// 初始化错误日志中间件
+        /// </summary>
+        /// <param name="next">方法</param>
+        public RemoteStopMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        /// <summary>
+        /// 执行方法
+        /// </summary>
+        /// <param name="context">Http上下文</param>
+        /// <param name="lifetime">应用程序生命周期</param>
+        public async Task Invoke(HttpContext context, IApplicationLifetime lifetime)
+        {
+            if (context.Request.Method == "HEAD" && context.Request.Headers[RequestHeader].FirstOrDefault() == "Yes")
+            {
+                context.Response.Headers.Add(ResponseHeader, "Yes");
+                lifetime.StopApplication();
+            }
+            else
+            {
+                await _next(context);
+            }
+        }
+
+    }
+}
