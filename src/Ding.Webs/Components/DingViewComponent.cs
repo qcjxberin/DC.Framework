0c38d87d42597f1cdab0eb0f8750e2129432135e
丁川 <2505111990@qq.com>
2019-05-13 17:46:46 +0800

    增加ViewComponent的基类

---
+++ src/Ding.Webs/Components/DingViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace Ding.Webs.Components
+{
+    /// <summary>
+    /// ViewComponent的基类
+    /// </summary>
+    public abstract class DingViewComponent : ViewComponent
+    {
+        
+    }
+}
