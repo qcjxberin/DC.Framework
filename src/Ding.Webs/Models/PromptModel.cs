cfbc217f58fe60b09a3bf59db7b580030910c9cf
丁川 <2505111990@qq.com>
2019-06-05 19:19:47 +0800

    调整提示信息模型

--- src/Ding.Webs/Models/PromptModel.cs
+++ src/Ding.Webs/Models/PromptModel.cs
@@ -8,36 +8,36 @@
         /// <summary>
         /// 返回地址
         /// </summary>
-        public string BackUrl { get; set; }
+        public string BackUrl { get; set; } = "";
 
         /// <summary>
         /// 提示信息
         /// </summary>
-        public string Message { get; set; }
+        public string Message { get; set; } = "";
 
         /// <summary>
         /// 倒计时时间
         /// </summary>
-        public int CountdownTime { get; set; }
+        public int CountdownTime { get; set; } = 0;
 
         /// <summary>
         /// 是否显示返回地址
         /// </summary>
-        public bool IsShowBackLink { get; set; }
+        public bool IsShowBackLink { get; set; } = false;
 
         /// <summary>
         /// 是否自动返回
         /// </summary>
-        public bool IsAutoBack { get; set; }
+        public bool IsAutoBack { get; set; } = true;
 
         /// <summary>
         /// 网页标题
         /// </summary>
-        public string Title { get; set; }
+        public string Title { get; set; } = "";
 
         /// <summary>
         /// 网站名称
         /// </summary>
-        public string WebName { get; set; }
+        public string WebName { get; set; } = "";
     }
 }
