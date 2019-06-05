0773c4003d4838977953e34a302bc6d434bfdae4
丁川 <2505111990@qq.com>
2019-06-05 13:29:47 +0800

    增加提取摘要方法，有是否清除Html代码参数

--- src/Ding/Extensions/Bases/StringExtensions.cs
+++ src/Ding/Extensions/Bases/StringExtensions.cs
@@ -639,6 +639,147 @@ namespace Ding.Extension
         }
         #endregion
 
+        /// <summary>
+        /// 提取摘要，是否清除HTML代码
+        /// </summary>
+        /// <param name="content"></param>
+        /// <param name="length"></param>
+        /// <param name="StripHTML"></param>
+        /// <returns></returns>
+        public static string GetContentSummary(string content, int length, bool StripHTML)
+        {
+            if (string.IsNullOrEmpty(content) || length == 0)
+                return "";
+            if (StripHTML)
+            {
+                Regex re = new Regex("<[^>]*>");
+                content = re.Replace(content, "");
+                content = content.Replace("　", "").Replace(" ", "");
+                if (content.Length <= length)
+                    return content;
+                else
+                    return content.Substring(0, length) + "……";
+            }
+            else
+            {
+                if (content.Length <= length)
+                    return content;
+
+                int pos = 0, npos = 0, size = 0;
+                bool firststop = false, notr = false, noli = false;
+                StringBuilder sb = new StringBuilder();
+                while (true)
+                {
+                    if (pos >= content.Length)
+                        break;
+                    string cur = content.Substring(pos, 1);
+                    if (cur == "<")
+                    {
+                        string next = content.Substring(pos + 1, 3).ToLower();
+                        if (next.IndexOf("p") == 0 && next.IndexOf("pre") != 0)
+                        {
+                            npos = content.IndexOf(">", pos) + 1;
+                        }
+                        else if (next.IndexOf("/p") == 0 && next.IndexOf("/pr") != 0)
+                        {
+                            npos = content.IndexOf(">", pos) + 1;
+                            if (size < length)
+                                sb.Append("<br/>");
+                        }
+                        else if (next.IndexOf("br") == 0)
+                        {
+                            npos = content.IndexOf(">", pos) + 1;
+                            if (size < length)
+                                sb.Append("<br/>");
+                        }
+                        else if (next.IndexOf("img") == 0)
+                        {
+                            npos = content.IndexOf(">", pos) + 1;
+                            if (size < length)
+                            {
+                                sb.Append(content.Substring(pos, npos - pos));
+                                size += npos - pos + 1;
+                            }
+                        }
+                        else if (next.IndexOf("li") == 0 || next.IndexOf("/li") == 0)
+                        {
+                            npos = content.IndexOf(">", pos) + 1;
+                            if (size < length)
+                            {
+                                sb.Append(content.Substring(pos, npos - pos));
+                            }
+                            else
+                            {
+                                if (!noli && next.IndexOf("/li") == 0)
+                                {
+                                    sb.Append(content.Substring(pos, npos - pos));
+                                    noli = true;
+                                }
+                            }
+                        }
+                        else if (next.IndexOf("tr") == 0 || next.IndexOf("/tr") == 0)
+                        {
+                            npos = content.IndexOf(">", pos) + 1;
+                            if (size < length)
+                            {
+                                sb.Append(content.Substring(pos, npos - pos));
+                            }
+                            else
+                            {
+                                if (!notr && next.IndexOf("/tr") == 0)
+                                {
+                                    sb.Append(content.Substring(pos, npos - pos));
+                                    notr = true;
+                                }
+                            }
+                        }
+                        else if (next.IndexOf("td") == 0 || next.IndexOf("/td") == 0)
+                        {
+                            npos = content.IndexOf(">", pos) + 1;
+                            if (size < length)
+                            {
+                                sb.Append(content.Substring(pos, npos - pos));
+                            }
+                            else
+                            {
+                                if (!notr)
+                                {
+                                    sb.Append(content.Substring(pos, npos - pos));
+                                }
+                            }
+                        }
+                        else
+                        {
+                            npos = content.IndexOf(">", pos) + 1;
+                            sb.Append(content.Substring(pos, npos - pos));
+                        }
+                        if (npos <= pos)
+                            npos = pos + 1;
+                        pos = npos;
+                    }
+                    else
+                    {
+                        if (size < length)
+                        {
+                            sb.Append(cur);
+                            size++;
+                        }
+                        else
+                        {
+                            if (!firststop)
+                            {
+                                sb.Append("……");
+                                firststop = true;
+                            }
+                        }
+                        pos++;
+                    }
+
+                }
+                return sb.ToString();
+            }
+        }
+
         #region WordCase(单词大小写)
         /// <summary>
         /// 首字母大写
