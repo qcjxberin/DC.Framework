f2399f1d27bbfbd2ba94b509c008085c0f3d7b78
丁川 <dingchuan@fivepower.net>
2019-07-18 15:44:17 +0800

    调整

--- src/Ding.Biz.OAuthLogin/Factories/LoginFactory.cs
+++ src/Ding.Biz.OAuthLogin/Factories/LoginFactory.cs
@@ -6,6 +6,7 @@ using Ding.Biz.OAuthLogin.WeChat.Configs;
 using Ding.Biz.OAuthLogin.Weibo.Configs;
 using Ding.Extension;
 using Ding.Helpers;
+using Ding.Log;
 using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
 using System.Linq;
